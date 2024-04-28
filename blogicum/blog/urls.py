858a47a64d2992e5900218d464f63437a607deb2
Nikita Noskov <nik030205@mail.ru>
2024-04-28 22:39:14 +0300

    Main page.

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from blog.views import index, category_posts, post_detail
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('posts/<int:id>/', post_detail, name='post_detail'),
+    path('category/<slug:category_slug>/', category_posts, name='category_posts')
+]
---
+++ blogicum/pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from pages.views import *
+
+
+app_name = 'pages'
+
+urlpatterns = [
+    
+]
